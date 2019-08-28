31f0ceca13014f4b0c31673bffe05a8903ab46f4
Jeong Park <wjdtn1220@gmail.com>
2019-08-28 01:31:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,12 +29,12 @@ class Song
   
   def self.genre_count
     genre_count = {}
-      @@genres.each do |genre|
-        if genre_count[genre]
-          genre_count[genre] += 1
-        else
-         genre_count[genre]
-         genre_count[genre] = 1
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre]
+        genre_count[genre] = 1
         end
       end
     genre_count
