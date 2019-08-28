67429a1835011c8a7aec5614b40c8a292dfbca85
Jeong Park <wjdtn1220@gmail.com>
2019-08-28 00:11:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,18 +3,21 @@ class Song
 attr_accessor :name, :artist, :genre
 
 @@count = 0
+@@artists = []
 
   def initialize
     @@count += 1
     @@genre = genre
+    @@artists
   end
   
   def self.count
    @@count 
   end
   
-  def self.artist
-    @@artist
-
+  def self.artists
+    @@artists.uniq
+  end
+  
 end
 
