cb5fbca719a3bf350e1e6b1bf2a8665b1c010e9a
Jeong Park <wjdtn1220@gmail.com>
2019-08-28 00:20:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,14 +28,14 @@ attr_accessor :name, :artist, :genre
   end
   
   def self.genre_count
-  genre_count = {}
-  @@genres.each do |genre|
-    if genre_count[genre]
-      genre_count += 1
-    else
-      genre_count[genre]
-      genre_count = 1
-    end
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count += 1
+      else
+        genre_count[genre]
+        genre_count = 1
+      end
   end
 end
 
